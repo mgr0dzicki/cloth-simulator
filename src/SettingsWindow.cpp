9d8ae86e80579de65c47929b05f7e6157581e8f3
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2024-01-19 23:24:20 +0100

    more settings mocks

--- src/SettingsWindow.cpp
+++ src/SettingsWindow.cpp
@@ -2,8 +2,26 @@
 #include <imgui.h>
 
 namespace Settings {
+enum class RenderMode { Points, Lines, Triangles };
+
 bool diagonalLinks = true;
 bool farLinks = true;
+bool clothClothCollision = true;
+RenderMode renderMode = RenderMode::Triangles;
+
+void recommended() {
+    diagonalLinks = true;
+    farLinks = true;
+    clothClothCollision = true;
+    renderMode = RenderMode::Triangles;
+}
+
+void allDisabled() {
+    diagonalLinks = false;
+    farLinks = false;
+    clothClothCollision = false;
+    renderMode = RenderMode::Points;
+}
 } // namespace Settings
 
 SettingsWindow::SettingsWindow(
@@ -19,10 +37,40 @@ void SettingsWindow::draw(float dt) {
 
     ImGui::Begin("Settings", NULL, ImGuiWindowFlags_AlwaysAutoResize);
     ImGui::Text("%.3f ms / frame", 1000.f * dt);
+    ImGui::Button("Reset animation");
+
     ImGui::Separator();
-    ImGui::Checkbox("Diagonal links", &Settings::diagonalLinks);
-    ImGui::Checkbox("Far links", &Settings::farLinks);
+
+    ImGui::Text("Render mode");
+    ImGui::RadioButton("Points", (int *)&Settings::renderMode,
+                       (int)Settings::RenderMode::Points);
+    ImGui::SameLine();
+    ImGui::RadioButton("Lines", (int *)&Settings::renderMode,
+                       (int)Settings::RenderMode::Lines);
+    ImGui::SameLine();
+    ImGui::RadioButton("Triangles", (int *)&Settings::renderMode,
+                       (int)Settings::RenderMode::Triangles);
+    ImGui::Spacing();
+
+    ImGui::Text("Links");
+    ImGui::Checkbox("Diagonal", &Settings::diagonalLinks);
+    ImGui::SameLine();
+    ImGui::Checkbox("Far", &Settings::farLinks);
+    ImGui::Spacing();
+
+    ImGui::Checkbox("Cloth-cloth collision", &Settings::clothClothCollision);
+    ImGui::Spacing();
+
     if (ImGui::SliderInt("Subdivision steps", &subdivisionSteps, 0, 3))
         subdivisionStepsCallback(subdivisionSteps);
+
+    ImGui::Spacing();
+
+    if (ImGui::Button("Recommended"))
+        Settings::recommended();
+    ImGui::SameLine();
+    if (ImGui::Button("All disabled"))
+        Settings::allDisabled();
+
     ImGui::End();
 }
