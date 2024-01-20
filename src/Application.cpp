fe67060f2e09198cd16359e5c9685a59c0f1abfd
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2024-01-20 22:54:34 +0100

    multisampling!

--- src/Application.cpp
+++ src/Application.cpp
@@ -45,6 +45,7 @@ Application::Application(std::string title)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 8 );
 
     // create the window
     window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
